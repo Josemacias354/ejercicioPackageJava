a0540549cc18fe3be75d1dc540a6b5347f91a096
Josemacias354 <maciasbichillo1@gmail.com>
2024-01-19 08:30:43 +0100

    Subo clases geometria y varias

---
+++ Geometria.java
@@ -0,0 +1,11 @@
+package Matematicas;
+
+public class Geometria {
+    public static double volumenCilindro(double r, double h) {
+        return Math.PI * r * r * h;
+    }
+    public static double longitudCircunferencia(double r) {
+        return 2 * Math.PI * r;
+    }
+}
+
